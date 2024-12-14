220331c8c4f89d336578f87ff5b9e79bec0550c0
BrenoRusso <breno.russoguedes@gmail.com>
2024-12-14 19:13:48 -0300

    Adicionando explicação a parte 1

--- AoC/AoC_2021/day3.lean
+++ AoC/AoC_2021/day3.lean
@@ -9,8 +9,8 @@ def content : String := include_str "../../data/AoC2021_day3.txt"
 
 Calcular o consumo de energia do submarino.
 
-
-
+O objetivo desta etapa é determinar duas taxas binárias: gamma rate e epsilon rate, com base na frequência dos bits em cada posição.
+O gamma rate é formado pelos bits mais frequentes em cada posição, enquanto o epsilon rate usa os menos frequentes. A multiplicação dessas taxas retorna o consumo de energia do submarino.
 -/
 
 
@@ -63,3 +63,45 @@ def maxbits (w : Nat) : List (Nat × Nat) → Nat
   let d := minbits (width - 1) merged
   let w := maxbits (width - 1) merged
   d * w
+
+
+/- # Part 2
+
+Calcular o consumo de energia do submarino.
+
+
+
+-/
+
+def string_to_nat_aux (w:Nat) : List Char → Nat
+| [] => 0
+| ('0'::xs) => string_to_nat_aux (w-1) xs
+| (_::xs) => 2^w + string_to_nat_aux (w-1) xs
+
+
+def string_to_nat (x:String) : Nat :=
+  string_to_nat_aux (x.length - 1) x.toList
+
+
+partial def bitfilter (f:Nat → Nat → Bool) (w:Nat) (lines: List String) : Nat :=
+  if lines.length == 1
+  then
+      string_to_nat lines.head!
+  else
+    let hist := (lines.map (λx => x.get w))
+    let ones := (hist.filter (λx => x == '1')).length
+    let zeros := (hist.filter (λx => x == '0')).length
+    let target := if f ones zeros then '1' else '0'
+    let valid := lines.filter (λx => x.get w == target)
+    bitfilter f (w+1) valid
+
+
+
+unsafe def main (args : List String) : IO UInt32 :=
+do let input ← IO.FS.lines "input.txt"
+   let lines := input.toList
+   let ogr := bitfilter (λx y => x >= y) 0 lines
+   let csr := bitfilter (λx y => x < y) 0 lines
+   IO.print (ogr * csr)
+   IO.print "\n"
+   return 0
